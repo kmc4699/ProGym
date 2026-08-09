a07251f33beff61d9237e9134d76c4f76c8a8c84
Srikar Kurani <srikarkurani@gmail.com>
2026-08-09 22:01:31 +1200

    Add Membership properties and constructor

--- GymManagement/Membership.cs
+++ GymManagement/Membership.cs
@@ -1,10 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GymManagement
 {
-    internal class Membership
+    public class Membership
     {
+        public string MemberId { get; }
+        public string MemberName { get; }
+        public DateTime ExpiryDate { get; private set; }
+
+        public Membership(string memberId, string memberName, DateTime expiryDate)
+        {
+            MemberId = memberId;
+            MemberName = memberName;
+            ExpiryDate = expiryDate;
+        }
     }
 }
