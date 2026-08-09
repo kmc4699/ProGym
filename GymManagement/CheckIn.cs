c04edb9fd0902f23b316edc92f7ddff61aec4271
Srikar Kurani <srikarkurani@gmail.com>
2026-08-09 22:03:54 +1200

    Add check-in class with validation logic for membership expiry

--- GymManagement/CheckIn.cs
+++ GymManagement/CheckIn.cs
@@ -1,10 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GymManagement
 {
-    internal class CheckIn
+    public class CheckIn
     {
+        public string MemberId { get; }
+        public DateTime CheckInTime { get; }
+
+        public CheckIn(Membership membership)
+        {
+            if (membership == null)
+                throw new ArgumentNullException(nameof(membership));
+
+            if (!membership.IsActive())
+                throw new InvalidOperationException("Cannot check in: membership has expired.");
+
+            MemberId = membership.MemberId;
+            CheckInTime = DateTime.Now;
+        }
     }
 }
