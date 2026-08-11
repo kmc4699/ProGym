b1a462e828fb7f0c7ca882ab6a56c983adba0e78
Srikar Kurani <srikarkurani@gmail.com>
2026-08-11 15:42:58 +1200

    Add unit tests for CheckIn class

---
+++ GymManagement.Tests/CheckInTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GymManagement;
+
+namespace GymManagement.Tests
+{
+    [TestClass]
+    public class CheckInTests
+    {
+        // Should be able to check in with an active membership
+        [TestMethod]
+        public void CheckIn_ActiveMembership_Succeeds()
+        {
+            var membership = new Membership("M1", "Jane Doe", DateTime.Now.AddDays(30));
+            var checkIn = new CheckIn(membership);
+            Assert.AreEqual("M1", checkIn.MemberId);
+        }
+
+        // Expired membership should block the check-in
+        [TestMethod]
+        public void CheckIn_ExpiredMembership_ThrowsException()
+        {
+            var membership = new Membership("M1", "Jane Doe", DateTime.Now.AddDays(-1));
+            Assert.ThrowsException<InvalidOperationException>(() => new CheckIn(membership));
+        }
+
+        // Check-in shouldn't work without a membership at all
+        [TestMethod]
+        public void CheckIn_NullMembership_ThrowsException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new CheckIn(null!));
+        }
+    }
+}
