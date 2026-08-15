03679c5aad011f95390d3cd98e2f583a2762c129
Srikar Kurani <srikarkurani@gmail.com>
2026-08-15 18:04:54 +1200

    Add tests for membership renewal

--- GymManagement.Tests/MembershipTests.cs
+++ GymManagement.Tests/MembershipTests.cs
@@ -7,7 +7,7 @@ namespace GymManagement.Tests
     [TestClass]
     public class MembershipTests
     {
-        // Membership should be active if it hasn't expired yet
+        // membership should be active if it hasn't expired yet
         [TestMethod]
         public void Membership_ActiveWhenExpiryInFuture_ReturnsTrue()
         {
@@ -15,7 +15,7 @@ namespace GymManagement.Tests
             Assert.IsTrue(membership.IsActive());
         }
 
-        // Membership should be inactive once the expiry date has passed
+        // membership should be inactive once the expiry date has passed
         [TestMethod]
         public void Membership_ExpiredWhenExpiryInPast_ReturnsFalse()
         {
@@ -23,12 +23,34 @@ namespace GymManagement.Tests
             Assert.IsFalse(membership.IsActive());
         }
 
-        // Cannot create a membership without a member ID
+        // can't create a membership without a member ID
         [TestMethod]
         public void Membership_EmptyMemberId_ThrowsException()
         {
             Assert.ThrowsExactly<ArgumentException>(() =>
-                   new Membership("", "Jane Doe", DateTime.Now.AddDays(30)));
+                new Membership("", "Jane Doe", DateTime.Now.AddDays(30)));
+        }
+
+        // renewing should push the expiry date further into the future
+        [TestMethod]
+        public void Membership_Renew_ExtendsExpiryDate()
+        {
+            var membership = new Membership("M1", "Jane Doe", DateTime.Now.AddDays(10));
+            var newExpiry = DateTime.Now.AddDays(40);
+
+            membership.Renew(newExpiry);
+
+            Assert.AreEqual(newExpiry, membership.ExpiryDate);
+        }
+
+        // Can't renew to an earlier date than the current expiry
+        [TestMethod]
+        public void Membership_Renew_EarlierDate_ThrowsException()
+        {
+            var membership = new Membership("M1", "Jane Doe", DateTime.Now.AddDays(30));
+
+            Assert.ThrowsExactly<ArgumentException>(() =>
+                membership.Renew(DateTime.Now.AddDays(5)));
         }
     }
 }
