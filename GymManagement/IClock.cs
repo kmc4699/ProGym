141e41224e6feea1dcd2ff9357ffeee7c2bffd0b
Srikar Kurani <srikarkurani@gmail.com>
2026-08-26 16:46:56 +1200

    Add IClock abstraction for date logic

---
+++ GymManagement/IClock.cs
@@ -0,0 +1,14 @@
+﻿namespace GymManagement
+{
+    // Lets us fake "today" in tests instead of relying on the real clock
+    public interface IClock
+    {
+        DateTime Today { get; }
+    }
+
+    // The real clock, used everywhere except tests
+    public class SystemClock : IClock
+    {
+        public DateTime Today => DateTime.Now.Date;
+    }
+}
